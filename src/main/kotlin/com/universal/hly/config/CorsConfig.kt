3a803aa69a2cdc5029d614c08f62354f12c8f83a
kevin <coinicon@163.com>
2018-04-15 13:11:13 +0800

    + enable cors for rest & data rest

---
+++ src/main/kotlin/com/universal/hly/config/CorsConfig.kt
@@ -0,0 +1,29 @@
+package com.universal.hly.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
+
+
+
+
+/**
+ * Created by swm on 2018-4-14
+ */
+@Configuration
+class CorsConfig : WebMvcConfigurerAdapter() {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")//.allowedOrigins("*");
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD")
+    }
+}
+
+@Configuration
+class DataRestCorsConfigurer : RepositoryRestConfigurerAdapter() {
+    override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration) {
+        config.corsRegistry.addMapping(config.basePath.path + "/**")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+    }
+}
